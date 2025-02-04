cf95ee74c170be36759f8f694ce5a4958caa16a8
Nimira43 <n.rabone72@aol.com>
2025-02-04 21:17:02 +0000

    Archiving the current index.js file - this has the custom bodyParser middleware.

---
+++ archive/01-Custom-BodyParser/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <h1>Ceapstow Wholesalers</h1>
+      <hr />
+      <h2>Register</h2>
+      <form method="POST">
+        <input 
+          name="name" 
+          placeholder="Name" 
+        />
+        <input 
+          name="email" 
+          placeholder="Email" 
+        />
+        <input 
+          name="password" 
+          placeholder="Password" 
+        />
+        <input 
+          name="passwordConfirmation"
+          placeholder="Password Confirm" 
+        />
+        <button>Register</button>
+      </form>
+    </div>
+  `)
+})
+
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', data => {
+      const parsed = data.toString('utf8').split('&')
+      const formData = {}
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=')
+        formData[key] = decodeURIComponent(value)
+      }
+      req.body  = formData
+      next()
+    })
+  } else {
+    next()
+  }
+}
+
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body)
+  res.send('Account Created!')
+})
+
+app.listen(3000, () => {
+  console.log('Server listening on Port 3000')
+})
