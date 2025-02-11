2d251c212b6c0955a85e5d06bb46967a5c223358
Nimira43 <n.rabone72@aol.com>
2025-02-11 16:25:15 +0000

    Checking to see if file exists

--- repositories/users.js
+++ repositories/users.js
@@ -1,9 +1,21 @@
+const fs = require('fs')
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
       throw new Error('Creating a repository requires a filename')
     }
+    this.filename = filename
+    try {
+      fs.accessSync(this.filename)
+    } catch (err) {
+
+    } 
   }
+  async checkForFile() {
+
+  }  
 }
 
-new UsersRepository()
+const repo = new UsersRepository()
+repo.checkForFile(0)
