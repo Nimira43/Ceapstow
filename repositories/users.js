c8d15c196ee4cf2dd38f2b48e05b8bdd84e87d4e
Nimira43 <n.rabone72@aol.com>
2025-02-11 18:12:25 +0000

    Brought in the randomId method into create method to create a user ID as well.

--- repositories/users.js
+++ repositories/users.js
@@ -18,6 +18,7 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId()
     const records = await this.getAll()
     records.push(attrs)
     await this.writeAll(records)  
