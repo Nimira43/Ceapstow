1935c3f147b996e78c86b9880c337d13d34b7ab2
Nimira43 <n.rabone72@aol.com>
2025-04-24 11:59:12 +0100

    Repeating the same HTML tags again and again isn't good sooo...... created views/admin/layout.js

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,31 +1,21 @@
 module.exports = () => {
   return `
-
-    <!DOCTYPE html>
-    <html>
-      <head>
-      
-      </head>
-      <body>
-        <div>
-          <link rel="stylesheet" type="text/css" href="styles.css">
-          <h1>Ceapstow Wholesalers</h1>
-          <hr />
-          <h2>Login</h2>
-          <form method="POST">
-            <input 
-              name="email" 
-              placeholder="Email" 
-            />
-            <input 
-              name="password" 
-              placeholder="Password" 
-            />
-            <button>Login</button>
-          </form>
-        </div>
-      </body>
-    </html>
-    
+    <div>
+      <link rel="stylesheet" type="text/css" href="styles.css">
+      <h1>Ceapstow Wholesalers</h1>
+      <hr />
+      <h2>Login</h2>
+      <form method="POST">
+        <input 
+          name="email" 
+          placeholder="Email" 
+        />
+        <input 
+          name="password" 
+          placeholder="Password" 
+        />
+        <button>Login</button>
+      </form>
+    </div>    
   `
 }
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,39 +1,31 @@
 module.exports = ({ req }) => {
   return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-      
-      </head>
-      <body>
-        <div>
-          <link rel="stylesheet" type="text/css" href="/styles.css">
-          <h1>Ceapstow Wholesalers</h1>
-          <hr />
-          <h4>Your ID is ${req.session.userId}</h4>
-          <hr />
-          <h2>Register</h2>
-          <form method="POST">
-            <input
-              name="name"
-              placeholder="Name"
-            />
-            <input
-              name="email"
-              placeholder="Email"
-            />
-            <input
-              name="password"
-              placeholder="Password"
-            />
-            <input
-              name="passwordConfirmation"
-              placeholder="Password Confirm"
-            />
-            <button>Register</button>
-          </form>
-        </div>
-      </body>
-    </html>
+    <div>
+    <link rel="stylesheet" type="text/css" href="/styles.css">
+      <h1>Ceapstow Wholesalers</h1>
+      <hr />
+      <h4>Your ID is ${req.session.userId}</h4>
+      <hr />
+      <h2>Register</h2>
+      <form method="POST">
+        <input
+          name="name"
+          placeholder="Name"
+        />
+        <input
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          name="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordConfirmation"
+          placeholder="Password Confirm"
+        />
+        <button>Register</button>
+      </form>
+    </div>
   `
 }
