b8c7a1e613d0503fbc5d3acb5caf182ad8bc62cb
Nimira43 <n.rabone72@aol.com>
2025-04-24 12:01:57 +0100

    Completed layout.js - for now

--- views/admin/layout.js
+++ views/admin/layout.js
@@ -1,3 +1,13 @@
 module.exports = ({ content }) => {
-
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>  
+  `
 }
