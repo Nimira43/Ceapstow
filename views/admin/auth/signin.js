34fa5a59271e86bd648b600443d08ce9b5a9e98d
Nimira43 <n.rabone72@aol.com>
2025-04-24 12:05:46 +0100

    Importing layout into signin.js and wrapping its content inside layout

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,21 +1,25 @@
+const layout = require('../layout')
+
 module.exports = () => {
-  return `
-    <div>
-      <link rel="stylesheet" type="text/css" href="styles.css">
-      <h1>Ceapstow Wholesalers</h1>
-      <hr />
-      <h2>Login</h2>
-      <form method="POST">
-        <input 
-          name="email" 
-          placeholder="Email" 
-        />
-        <input 
-          name="password" 
-          placeholder="Password" 
-        />
-        <button>Login</button>
-      </form>
-    </div>    
-  `
+  return layout({
+    content: `
+      <div>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <h1>Ceapstow Wholesalers</h1>
+        <hr />
+        <h2>Login</h2>
+        <form method="POST">
+          <input 
+            name="email" 
+            placeholder="Email" 
+          />
+          <input 
+            name="password" 
+            placeholder="Password" 
+          />
+          <button>Login</button>
+        </form>
+      </div>    
+    `
+  })
 }
