cacdda0e7a19032ce9dc13210d7361ff9d0a2920
Nimira43 <n.rabone72@aol.com>
2025-04-24 11:48:57 +0100

    Adding HTML markup to templates. Cleaned up users.json.

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,21 +1,31 @@
 module.exports = () => {
   return `
-    <div>
-      <link rel="stylesheet" type="text/css" href="styles.css">
-      <h1>Ceapstow Wholesalers</h1>
-      <hr />
-      <h2>Login</h2>
-      <form method="POST">
-        <input 
-          name="email" 
-          placeholder="Email" 
-        />
-        <input 
-          name="password" 
-          placeholder="Password" 
-        />
-        <button>Login</button>
-      </form>
-    </div>
+
+    <!DOCTYPE html>
+    <html>
+      <head>
+      
+      </head>
+      <body>
+        <div>
+          <link rel="stylesheet" type="text/css" href="styles.css">
+          <h1>Ceapstow Wholesalers</h1>
+          <hr />
+          <h2>Login</h2>
+          <form method="POST">
+            <input 
+              name="email" 
+              placeholder="Email" 
+            />
+            <input 
+              name="password" 
+              placeholder="Password" 
+            />
+            <button>Login</button>
+          </form>
+        </div>
+      </body>
+    </html>
+    
   `
 }
