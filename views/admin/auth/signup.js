bb01c834d02453839304919d7826b1dd9fbe083a
Nimira43 <n.rabone72@aol.com>
2025-04-24 12:08:02 +0100

    Done the same with signup.js

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,31 +1,35 @@
+const layout = require('../layout')
+
 module.exports = ({ req }) => {
-  return `
-    <div>
-    <link rel="stylesheet" type="text/css" href="/styles.css">
-      <h1>Ceapstow Wholesalers</h1>
-      <hr />
-      <h4>Your ID is ${req.session.userId}</h4>
-      <hr />
-      <h2>Register</h2>
-      <form method="POST">
-        <input
-          name="name"
-          placeholder="Name"
-        />
-        <input
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          name="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordConfirmation"
-          placeholder="Password Confirm"
-        />
-        <button>Register</button>
-      </form>
-    </div>
-  `
+  return layout({
+    content: `
+      <div>
+      <link rel="stylesheet" type="text/css" href="/styles.css">
+        <h1>Ceapstow Wholesalers</h1>
+        <hr />
+        <h4>Your ID is ${req.session.userId}</h4>
+        <hr />
+        <h2>Register</h2>
+        <form method="POST">
+          <input
+            name="name"
+            placeholder="Name"
+          />
+          <input
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            name="password"
+            placeholder="Password"
+          />
+          <input
+            name="passwordConfirmation"
+            placeholder="Password Confirm"
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    `
+  })
 }
