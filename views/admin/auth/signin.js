05a272f5f2b0db476eec6448c8f81c76246ece9d
Nimira43 <n.rabone72@aol.com>
2025-06-09 00:41:46 +0100

    Updating signin.js

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -4,16 +4,18 @@ const {getError} = require('../../helpers')
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      <div>
-        <h1>Ceapstow Wholesalers</h1>
-        <h2>Login</h2>
-        <form method="POST">
-          <input name="email" placeholder="Email" />
-          ${getError(errors, 'email')}
-          <input name="password" placeholder="Password" />
-          ${getError(errors, 'password')}
-          <button>Login</button>
-        </form>
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-one-quarter">
+            <form method="POST">
+              <input name="email" placeholder="Email" />
+              ${getError(errors, 'email')}
+              <input name="password" placeholder="Password" />
+              ${getError(errors, 'password')}
+              <button>Login</button>
+            </form>
+          </div>
+        </div>
       </div>    
     `
   })
