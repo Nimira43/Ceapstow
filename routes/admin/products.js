56a7c6601a35363b3f75d7397ba215746b7295fd
Nimira43 <n.rabone72@aol.com>
2025-07-25 22:29:24 +0100

    Will define products routes later.

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/admin/products', (req, res) => {})
-router.get('/admin/products/new', (req, res) => {})
+router.get('/admin/products', (req, res) => {
+  
+})
+router.get('/admin/products/new', (req, res) => {
+
+})
 
 modules.exports = router
