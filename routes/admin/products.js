dab94299723c5fbbe61edb5e0cd215ac46dcc0a1
Nimira43 <n.rabone72@aol.com>
2025-07-25 22:31:30 +0100

    Check commit

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,10 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/admin/products', (req, res) => {
-  
 })
 router.get('/admin/products/new', (req, res) => {
-
 })
 
 modules.exports = router
