d491990072b618b0be8d65ed699bd8854ef57555
Nimira43 <n.rabone72@aol.com>
2025-08-08 23:19:40 +0100

    Placed create method back in UsersRepository

--- repositories/users.js
+++ repositories/users.js
@@ -5,7 +5,20 @@ const util = require('util')
 const scrypt = util.promisify(crypto.scrypt)
 
 class UsersRepository {
-  
+  async create(attrs) {
+    attrs.id = this.randomId()
+    const salt = crypto.randomBytes(8).toString('hex')
+    const buf = await scrypt(attrs.password, salt, 64)
+
+    const records = await this.getAll()
+    const record = {
+      ...attrs,
+      password: `${buf.toString('hex')}.${salt}`
+    }
+    records.push(record)
+    await this.writeAll(records)
+    return record
+  }
 }
 
 module.exports = new UsersRepository('users.json')
