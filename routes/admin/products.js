9cf98f3b55b9462ce5a485427467fc5ae155d813
Nimira43 <n.rabone72@aol.com>
2025-10-28 04:19:25 +0000

    Bringing in multer into products.js

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,6 +3,7 @@ const productsRepo = require('../../repositories/products')
 const productsNewTemplate = require('../../views/admin/products/new')
 const { requireTitle, requirePrice } = require('./validators')
 const { validationResult } = require('express-validator')
+const multer = require('multer')
 
 const router = express.Router()
 
