6526f9b31238eb1d146842d33ee759c8af887fd7
Nimira43 <n.rabone72@aol.com>
2025-10-28 03:21:28 +0000

    THatshould be ValidationResult not Request

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,7 +2,7 @@ const express = require('express')
 const productsRepo = require('../../repositories/products')
 const productsNewTemplate = require('../../views/admin/products/new')
 const { requireTitle, requirePrice } = require('./validators')
-const { validationRequest } = require('express-validator')
+const { validationResult } = require('express-validator')
 
 const router = express.Router()
 
@@ -17,6 +17,7 @@ router.post(
   '/admin/products/new', 
   [requireTitle, requirePrice],
   (req, res) => {
+
     console.log()
     res.send('Submitted!')
   }
