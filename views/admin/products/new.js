e02258e8b85528e97f00982c1cc255a902ad0134
Nimira43 <n.rabone72@aol.com>
2025-12-02 19:46:47 +0000

    Styling Products form - 1

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,17 +4,22 @@ const { getError } = require('../../helpers')
 module.exports = ({ errors }) => {
   return layout ({
     content: `
-      <form 
-        method="POST"
-        enctype="multipart/form-data"
-      >
-        <input placeholder="Title" name="title" />
-        ${getError(errors, 'title')}
-        <input placeholder="Price" name="price" />
-        ${getError(errors, 'price')}
-        <input type="file" name="image" />
-        <button>Submit</button>
-      </form>
+      <div>
+        <div>
+          <h1></h1>
+          <form 
+            method="POST"
+            enctype="multipart/form-data"
+          >
+            <input placeholder="Title" name="title" />
+            ${getError(errors, 'title')}
+            <input placeholder="Price" name="price" />
+            ${getError(errors, 'price')}
+            <input type="file" name="image" />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>      
     `
   })
 }
