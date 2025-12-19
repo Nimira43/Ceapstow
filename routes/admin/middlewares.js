247a268a520029046ad8bc071c2dad0ffe1f03b8
Nimira43 <n.rabone72@aol.com>
2025-12-19 19:02:58 +0000

    handleErrors - 2

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -3,7 +3,11 @@ const { validationResult } = require('express-validator')
 module.exports = {
   handleErrors(templateFunc) {
     return (req, res, next) => {
+      const errors = validationResult(req)
 
+      if (!errors.isEmpty()) {
+        return res.send(templateFunc({ errors }))
+      }
     }
   }  
 }
