f0dcd1c3ed4c207c5a7b4b6542620c0e957481de
Nimira43 <n.rabone72@aol.com>
2025-12-19 19:10:03 +0000

    Calling handleErrors

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -20,13 +20,8 @@ router.post(
   '/admin/products/new',
   upload.single('image'), 
   [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
   async (req, res) => {
-    const errors = validationResult(req)
-
-    if (!errors.isEmpty()) {
-      return res.send(productsNewTemplate({ errors }))
-    }
-
     const image = req.file.buffer.toString('base64')
     const { title, price } = req.body
     await productsRepo.create({ title, price, image })
